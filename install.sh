157dbf3879a335a1ae3dd841530f448e2c962ced
Simetraa <simetra@simetra.dev>
2021-07-14 16:54:38 +0100

    Remove unused gh cli key

--- install.sh
+++ install.sh
@@ -18,8 +18,3 @@ ssh-import-id-gh $GITHUB_USERNAME
 echo Installing packages...
 
 sudo apt install w3m w3m-img tree exa gh screen dirmngr htop --yes
-
-
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-
