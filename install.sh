cade5169d0f2a021292bf5627c3c1f71223afb01
Simetraa <simetra@simetra.dev>
2021-07-14 17:09:28 +0100

    Added ssh-import-id-gh package

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ ln -svf $(pwd)/.bash_aliases ~/.bash_aliases
 ln -svf $(pwd)/.gitconfig ~/.gitconfig
 ln -svf $(pwd)/.profile ~/.profile
 
+echo Installing packages...
+
+sudo apt install ssh-import-id-gh: ssh git w3m w3m-img tree snap screen dirmngr htop --yes
+sudo snap install gh exa
+
 echo Installing pubkeys...
 
 gpg --keyserver keyserver.ubuntu.com --recv FEF981DF2ABB975C
 GITHUB_USERNAME="Simetraa"
 ssh-import-id-gh $GITHUB_USERNAME
-
-echo Installing packages...
-
-sudo apt install ssh git w3m w3m-img tree snap screen dirmngr htop --yes
-sudo snap install gh exa
