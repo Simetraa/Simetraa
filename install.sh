014a65d55a74c8b1b762041aee8e35cf61114ebb
Simetraa <simetra@simetra.dev>
2021-07-21 17:54:35 +0100

    Install chrome and vscode

--- install.sh
+++ install.sh
@@ -26,6 +26,10 @@ read -p "Is this a desktop installation? [Y/N] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+        wget -O /tmp/chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+        sudo apt install /tmp/chrome.deb
+        wget -O /tmp/code.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+        sudo apt install /tmp/code.deb
         wget -O /tmp/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
         sudo apt install /tmp/discord.deb
         sudo snap install authy
