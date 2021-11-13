87d49a7e3195b3b2e891ff2c66af4213bff9ae5b
Simetraa <simetra@simetra.dev>
2021-11-13 23:19:50 +0000

    Add transfer.sh alias

---
+++ fish/functions/transfer.fish
@@ -0,0 +1,12 @@
+# https://github.com/dutchcoders/transfer.sh/blob/main/examples.md#aliases
+function transfer --description 'Upload a file to transfer.sh'
+    if [ $argv[1] ]
+        # write to output to tmpfile because of progress bar
+        set -l tmpfile ( mktemp -t transferXXXXXX )
+        curl --progress-bar --upload-file "$argv[1]" https://transfer.sh/(basename $argv[1]) >> $tmpfile
+        cat --plain $tmpfile
+        command rm -f $tmpfile
+    else
+        echo 'usage: transfer FILE_TO_TRANSFER'
+    end
+end
